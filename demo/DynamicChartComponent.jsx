2ebf5860684e6983f20b58320ab8d4bdc33c8929
hustcc <i@atool.org>
2016-07-22 12:06:27 +0800

    调整参数

--- demo/DynamicChartComponent.jsx
+++ demo/DynamicChartComponent.jsx
@@ -68,7 +68,7 @@ const DynamicChartComponent = React.createClass({
             visualMap: {
                 show: false,
                 min: 0,
-                max: 500,
+                max: 1000,
                 color: ['#BE002F', '#F20C00', '#F00056', '#FF2D51', '#FF2121', '#FF4C00', '#FF7500', 
                         '#FF8936', '#FFA400', '#F0C239', '#FFF143', '#FAFF72', '#C9DD22', '#AFDD22',
                         '#9ED900', '#00E500', '#0EB83A', '#0AA344', '#0C8918', '#057748', '#177CB0']
