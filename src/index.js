4944383d5a5fd0a4ca86316684ee06001e7c73b7
kenny <is99zsq@gmail.com>
2017-06-16 11:30:29 +0800

    Solve ref getEchartsInstance error issue

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import echarts from 'echarts';
 import Core from './core';
 
 export default class Chart extends React.Component {
-	render = () => (
-		<Core {...this.props} ref="core" echarts={echarts} />
-	)
+
+	render = () => <Core {...this.props} ref="core" echarts={echarts} />
+	
 	getEchartsInstance = () =>this.refs.core.getEchartsInstance();
 }
