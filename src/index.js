e93cb699209170f860be77433160255b5803cc00
kenny <is99zsq@gmail.com>
2017-06-16 11:25:46 +0800

    Ok

--- src/index.js
+++ src/index.js
@@ -3,15 +3,9 @@ import echarts from 'echarts';
 
 import Core from './core';
 
-//const Chart = props => (
-//  <Core {...props} echarts={echarts} />
-//);
-
 export default class Chart extends React.Component {
-
 	render = () => (
 		<Core {...this.props} ref="core" echarts={echarts} />
 	)
 	getEchartsInstance = () =>this.refs.core.getEchartsInstance();
-
 }
