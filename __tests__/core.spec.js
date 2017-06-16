c22f0d51e067471a7945ebb94f9748490eb2d109
hustcc <xiaowei.wzw@antfin.com>
2017-06-16 15:57:24 +0800

    add testcases

--- __tests__/core.spec.js
+++ __tests__/core.spec.js
@@ -7,10 +7,9 @@ import ReactEcharts from '../src/core';
 import option from './option';
 
 test('test echarts-for-react\'s core.js.', () => {
-  const component = shallow(
+  let component = shallow(
     <ReactEcharts
       option={option}
-      style={{height: '400px'}}
       className="echarts-for-react"
       echarts={echarts}
     />
@@ -23,4 +22,41 @@ test('test echarts-for-react\'s core.js.', () => {
   expect(component.hasClass('echarts-for-react')).toBe(true);
 
   expect(component.type()).toEqual('div');
+  // props
+  expect(component.instance().props.option).toEqual(option);
+  expect(component.instance().props.style).toEqual({height: '300px'});
+  expect(component.instance().props.className).toEqual('echarts-for-react');
+  expect(component.instance().props.notMerge).toEqual(false);
+  expect(component.instance().props.lazyUpdate).toEqual(false);
+  expect(component.instance().props.theme).toEqual(null);
+  expect(typeof component.instance().props.onChartReady).toEqual('function');
+  expect(component.instance().props.showLoading).toEqual(false);
+  expect(component.instance().props.onEvents).toEqual({});
+
+
+  const testFunc = () => {};
+  // not default props
+  component = shallow(
+    <ReactEcharts
+      option={option}
+      style={{ width: 100 }}
+      notMerge
+      lazyUpdate
+      theme="test_theme"
+      onChartReady={testFunc}
+      showLoading
+      onEvents={{ onClick: testFunc }}
+      className="echarts-for-react"
+    />
+  );
+
+  expect(component.instance().props.option).toEqual(option);
+  expect(component.instance().props.style).toEqual({width: 100});
+  expect(component.instance().props.className).toEqual('echarts-for-react');
+  expect(component.instance().props.notMerge).toEqual(true);
+  expect(component.instance().props.lazyUpdate).toEqual(true);
+  expect(component.instance().props.theme).toEqual('test_theme');
+  expect(component.instance().props.onChartReady).toEqual(testFunc);
+  expect(component.instance().props.showLoading).toEqual(true);
+  expect(component.instance().props.onEvents).toEqual({ onClick: testFunc });
 });
