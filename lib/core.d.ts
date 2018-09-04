d5326a2381a95fd97807fc78d9fe0569d9fcacab
xsthunder <xsthunder@outlook.com>
2018-09-04 11:01:58 +0800

    add core.d.ts for ReactEchartsCore for ts

---
+++ lib/core.d.ts
@@ -0,0 +1,39 @@
+/// <reference types="react" />
+
+import * as React from 'react';
+
+type Func = (...args: any[]) => any;
+
+interface EventMap {
+  [key: string]: Func,
+}
+
+interface ObjectMap {
+  [key: string]: any,
+}
+
+interface optsMap {
+  devicePixelRatio?: number,
+  renderer?: 'canvas' | 'svg',
+  width?: number | null | undefined | 'auto',
+  height?: number | null | undefined | 'auto',
+}
+
+// Index
+export interface ReactEchartsPropsTypes {
+  option: ObjectMap;
+  notMerge?: boolean;
+  lazyUpdate?: boolean;
+  style?: ObjectMap;
+  className?: string;
+  theme?: string | null;
+  onChartReady?: Func;
+  showLoading?: boolean;
+  loadingOption?: ObjectMap;
+  onEvents?: EventMap;
+  echarts?: object;
+  opts?: optsMap;
+  shouldSetOption?: Func;
+}
+
+export default class ReactEcharts extends React.Component<ReactEchartsPropsTypes, any>{}
