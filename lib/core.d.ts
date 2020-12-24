acd5886008167756923f88799f8bbe454b97abf1
王永续 <1341925417@qq.com>
2020-12-24 14:44:22 +0800

    fixed: ts

--- lib/core.d.ts
+++ lib/core.d.ts
@@ -1,11 +1,16 @@
 /// <reference types="react" />
 
 import * as React from 'react';
-import {ReactEchartsPropsTypes} from './index.d'
+import { ReactEchartsPropsTypes } from './index.d'
+import { ECharts } from 'echarts';
 
 // Core
 export interface ReactEchartsCorePropsTypes extends ReactEchartsPropsTypes{
   echarts: object;
 }
 
-export default class ReactEchartsCore extends React.Component<ReactEchartsCorePropsTypes, any>{}
+declare class ReactEchartsCore extends React.Component<ReactEchartsPropsTypes, any>{
+  getEchartsInstance(): ECharts;
+}
+
+export default ReactEchartsCore
