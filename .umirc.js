4fcc8bd0eb925f4c0f7986e6dfacaab9db15a01c
hustcc <i@hust.cc>
2021-02-07 19:23:21 +0800

    fix(docs): base url 404

--- .umirc.js
+++ .umirc.js
@@ -4,8 +4,9 @@ export default defineConfig({
   mode: 'site',
   title: '\b',
   base: '/',
-  publicPath: process.env.NODE_ENV === 'production' ? '/echarts-for-react/' : '/',
   exportStatic: {},
+  publicPath: process.env.NODE_ENV === 'production' ? '/echarts-for-react/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/echarts-for-react/' : '/',
   logo: 'https://cdn.jsdelivr.net/gh/apache/echarts-website@asf-site/zh/images/logo.png?_v_=20200710_1',
   styles: [
     '.__dumi-default-navbar-logo:not([data-plaintext]) { padding-left: 200px!important; }',
@@ -14,6 +15,7 @@ export default defineConfig({
   ],
   navs: [
     null,
+    { title: 'G2Plot', path: 'https://github.com/antvis/G2Plot' },
     { title: '在线文档', path: 'https://github.com/hustcc/echarts-for-react' },
     { title: 'GitHub', path: 'https://github.com/hustcc/echarts-for-react' },
   ],
