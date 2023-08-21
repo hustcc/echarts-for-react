ea46c59bf621d80eca83b72c68638632d99ecdb0
xwbxxx <49603007+xwbxxx@users.noreply.github.com>
2023-08-22 03:02:40 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -10,6 +10,7 @@ export type Opts = {
   readonly width?: number | null | undefined | 'auto';
   readonly height?: number | null | undefined | 'auto';
   readonly locale?: string;
+  readonly resize?: objective;
 };
 
 export type EChartsReactProps = {
